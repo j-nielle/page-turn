31503e1de30d0671be4473db2b7bb97f18e5d34e
j-nielle <rabayaangel@gmail.com>
2025-02-22 15:19:30 +0800

    chore: constants

---
+++ src/lib/constants.ts
@@ -0,0 +1,15 @@
+export const quillToolbar = [
+	[{ size: [] }],
+	["bold", "italic", "underline", "strike"],
+	[{ color: [] }, { background: [] }],
+	[{ script: "sub" }, { script: "super" }],
+	[{ header: 1 }, { header: 2 }, "blockquote"],
+	[
+		{ list: "ordered" },
+		{ list: "bullet" },
+		{ indent: "-1" },
+		{ indent: "+1" },
+	],
+	[{ direction: "rtl" }, { align: [] }],
+	["link", "image", "video"],
+]
