3e592a6d962896d27e6d3359503cad1b005adea0
j-nielle <rabayaangel@gmail.com>
2025-02-22 15:40:51 +0800

    chore: @vercel/analytics

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { Analytics } from "@vercel/analytics/react";
 import { HeroUIProvider } from "@heroui/react";
 import Header from "./Header";
 import Footer from "./Footer";
@@ -6,6 +7,7 @@ import Footer from "./Footer";
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <>
+      <Analytics />
       <HeroUIProvider>
         <Header />
         <main className="flex flex-col p-5 md:px-10 md:py-5 min-h-screen">
