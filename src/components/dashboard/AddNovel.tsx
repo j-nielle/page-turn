eafae7ba40bef554bcb8b3b4f6627f1c757dbeef
j-nielle <rabayaangel@gmail.com>
2025-02-27 01:52:07 +0800

    refactor: add novel component props

--- src/components/dashboard/AddNovel.tsx
+++ src/components/dashboard/AddNovel.tsx
@@ -4,11 +4,16 @@ import React from "react";
 import Modal from "@/components/modals/Modal";
 import { useDisclosure, Button } from "@heroui/react";
 import { NovelForm } from "@/components/forms";
+import { cn } from "@/lib/utils";
 
-export default function AddNovel() {
+interface AddNovelProps {
+  className?: string;
+}
+
+export default function AddNovel({className}: AddNovelProps) {
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
   return (
-    <div className="w-full">
+    <div className={cn("w-full p-2 border-2 border-default-200", className)}>
       <Button color="primary" className="w-full" onPress={onOpen}>
         Add Novel
       </Button>
