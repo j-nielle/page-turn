df81b36c7b87ead5cd840b432bf0b3531a6270f4
j-nielle <rabayaangel@gmail.com>
2025-02-26 20:01:23 +0800

    refactor: star rating component

--- src/components/user-activity/StarRating.tsx
+++ src/components/user-activity/StarRating.tsx
@@ -1,6 +1,38 @@
 import { cn } from "@/lib/utils";
 import React, { useId } from "react";
 
+interface StarProps {
+  filled: boolean;
+  onClick: () => void;
+  bgColor?: string;
+}
+
+const Star = ({ filled, onClick, bgColor = "#f4f4f4" }: StarProps) => {
+  const id = useId();
+
+  return (
+    <svg
+      id={id}
+      viewBox="0 0 22 22"
+      stroke="#9c7000"
+      xmlns="http://www.w3.org/2000/svg"
+      onClick={onClick}
+      width="24"
+      height="24"
+      style={{ cursor: "pointer" }}>
+      <defs>
+        <linearGradient id="starGradient">
+          <stop offset="100%" stopColor="#f5b100" />
+        </linearGradient>
+      </defs>
+      <path
+        d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"
+        fill={filled ? "url(#starGradient)" : bgColor}
+      />
+    </svg>
+  );
+};
+
 interface StarRatingProps {
   className?: string;
   totalStars?: number;
@@ -16,8 +48,6 @@ const StarRating = ({
   reviewsUrl = "",
   setRating,
 }: StarRatingProps) => {
-  const id = useId();
-
   const handleRating = (index: number) => {
     if (setRating) {
       setRating(index);
@@ -31,24 +61,11 @@ const StarRating = ({
       })}>
       <div className={cn("flex flex-row gap-1 items-center")}>
         {[...Array(totalStars)].map((_, index) => (
-          <svg
+          <Star
             key={index}
-            viewBox="0 0 22 22"
-            stroke="#9c7000"
-            xmlns="http://www.w3.org/2000/svg"
+            filled={index < rating}
             onClick={() => handleRating(index)}
-            width="24"
-            height="24">
-            <defs>
-              <linearGradient id={`${id}-star-${index}`}>
-                <stop offset="100%" stopColor="#f5b100" />
-              </linearGradient>
-            </defs>
-            <path
-              d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"
-              fill={index < rating ? `url(#${id}-star-${index})` : "#f4f4f4"}
-            />
-          </svg>
+          />
         ))}
         {reviewsUrl !== "" && (
           <>
