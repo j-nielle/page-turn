2889c69cd8818f04ce171fb8145f3851065035f4
j-nielle <rabayaangel@gmail.com>
2025-02-26 13:29:56 +0800

    chore: barrel file for novel components

---
+++ src/components/novel/index.ts
@@ -0,0 +1,6 @@
+export { default as Recommendations } from "@/components/novel/Recommendations";
+export { default as RecentlyUpdated } from "@/components/novel/RecentlyUpdated";
+export { default as NovelList } from "@/components/novel/NovelList";
+export { default as NovelCard } from "@/components/novel/NovelCard";
+export { default as NovelListItem } from "@/components/novel/NovelListItem";
+export { default as FilterNovels } from "@/components/novel/FilterNovels";
