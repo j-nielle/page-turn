090c1b82c5c5aa3c1dac39d4b7263c009e57b390
j-nielle <rabayaangel@gmail.com>
2025-02-27 01:48:33 +0800

    chore: added image and toggle components

--- src/components/ui/image.tsx
+++ src/components/ui/image.tsx
@@ -1,49 +1,38 @@
 "use client";
 
 import React, { useId } from "react";
-import NextImage from "next/image";
-import {
-  Image as HeroUIImage,
-  type ImageProps as HeroUIImageProps,
-} from "@heroui/react";
+import Image from "next/image";
+import type { ImageProps as NextImageProps } from "next/image";
 import { cn } from "@/lib/utils";
 
-interface ImageProps extends Partial<HeroUIImageProps> {
+type ImageProps = Partial<NextImageProps> & React.ComponentProps<"img"> & {
   url?: string;
-  width?: number;
-  height?: number;
   text?: string;
-  alt?: string;
-  key?: React.Key;
-}
+};
 
-const Image = (props: ImageProps) => {
+const ImageComponent = (props: ImageProps) => {
   const id = useId();
   return (
     <div className="sticky" key={props.key}>
-      <HeroUIImage
+      <Image
         id={id}
         ref={props.ref}
-        as={NextImage}
         alt={props.alt ?? "Novel Image"}
         height={props.height}
         width={props.width}
-        isBlurred
+        fill
         src={
           props.url ??
           `https://placehold.co/${props.width}x${props.height}.png?text=${props.text}`
         }
         className={cn(
           "rounded-lg antialiased object-cover transition-opacity duration-100 hover:opacity-75",
-          props.className, 
-          // {
-          //   "bg-blur": !props.url
-          // },
+          props.className,
         )}
       />
     </div>
   );
 };
-Image.displayName = "Image";
+ImageComponent.displayName = "Image";
 
-export { Image };
+export { ImageComponent as Image };
