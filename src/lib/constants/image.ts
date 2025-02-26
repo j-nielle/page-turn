934a5d23016ca2c9fbf3e5ccd54c59a99daa7fab
j-nielle <rabayaangel@gmail.com>
2025-02-27 01:47:47 +0800

    chore: updated imageSizes items

--- src/lib/constants/image.ts
+++ src/lib/constants/image.ts
@@ -1,7 +1,6 @@
 import { ImageSize } from "@/lib/types/image";
 
 export const imageSizes: ImageSize[] = [
-	{ width: 128, height: 179 },
-	{ width: 192, height: 269 },
-	{ width: 256, height: 358 },
+	{ width: 100, height: 180 },
+	{ width: 190, height: 250 },
 ]
