1c6bc9fe54dbd12841e4e47574066f86ca76f58c
j-nielle <rabayaangel@gmail.com>
2025-02-26 13:33:23 +0800

    chore: minor update in dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,18 +1,19 @@
 import React from "react";
 import Link from "next/link";
-import CreateNovel from '@/components/dashboard/CreateNovel';
+import AddNovel from '@/components/dashboard/AddNovel';
 
 export default function Page() {
   return (
-    <>
+    <div>
       <div>list of chapters in this novel</div>
-      <p>modal for adding a new novel</p>
-      <CreateNovel />
+      <div className="px-4 py-2 w-full border-2 border-default-200">
+      <AddNovel />
+      </div>
       <Link
         href="/dashboard/1/add-chapter"
         className="text-blue-500 hover:underline">
         Add Chapter to Novel ID: 1
       </Link>
-    </>
+    </div>
   );
 }
